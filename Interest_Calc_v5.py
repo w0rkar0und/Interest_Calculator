188560e00d0ecc6d9e386a3c5b2526f1173528c9
w0rkar0und <miten@mitenpatel.com>
2013-12-21 19:02:24 +0800

    update

--- Interest_Calc_v5.py
+++ Interest_Calc_v5.py
@@ -46,15 +46,27 @@ Created on 17 Dec 2013
 # Call Type is SPN argument passed on calcStdInt() function - will be either 
 # single SPN or *ALL
 #===============================================================================
-
+#===============================================================================
+# v4.1 Iteration
+# Functionality:
+#
+# Fixed calcIBB function - calculation of Formula F IBB was pointing to OTE 
+# rather than TE.
+# Fixed buildSPNDict to strip the white space around SPN values so that look up
+# of SPN works if value is shorter than stored value
+# Fixed buildBalDict to strip the white space around SPN values so that look up
+# of SPN works if value is shorter than stored value
+# Added IBB Type value to the output
+#===============================================================================
 
 #imports
 
 import csv
+import collections as col
 import time
 import datetime
 from Balances import Balances
-from SPNs import SPNStatic
+import hashlib
 
 #Base Interest Rate
 
@@ -65,25 +77,18 @@ int_base_rate = 1.25
 def buildSPNDict(SPN):
     global SPNs
     filename = "SPNs.csv"
-    b = open(filename, 'rb')
-    reader = csv.reader(b, quoting=csv.QUOTE_NONNUMERIC)
-    SPNs = []
 
     if SPN.upper() == "*ALL":
-        print "if"
-        #SPNs = [SPNStatic(row) for row in reader]
-        for row in reader:
-            SPNs.append(SPNStatic(row))
-        
+        f = open(filename,'rb')
+        SPN1 = csv.reader(f,quoting=csv.QUOTE_NONNUMERIC)
+        SPNs = col.OrderedDict((row[0].strip(), row[1:]) for row in SPN1)
     else:
-
-        for row in reader:
-
-            if row[0].strip() == SPN:
-                SPNs.append(SPNStatic(row))
+        f = open(filename, 'rb')
+        SPN1 = csv.reader(f,quoting=csv.QUOTE_NONNUMERIC)
+        rows = [row for row in SPN1 if row[0].strip() == SPN]
+        SPNs = col.OrderedDict((row[0].strip(), row[1:]) for row in rows)
     
     return SPNs
-    
 
 def buildBalDict(SPN):
     global accts_bal
@@ -99,88 +104,86 @@ def buildBalDict(SPN):
     else:
 
         for row in reader:
-
             if row[0].strip() == SPN:
                 accts_bal.append(Balances(row))
-        
+    
     return accts_bal
-
-
-def getFormula(SPN):
-    
-    for sublist in SPNs:
-
-        if sublist.spn == SPN:
-            print 'if'
-            print "CLIENT SPN:",SPN
-            print "Formula:",sublist.formula
-            int_formula = sublist.formula
-
-        else:
-            
-            print "SPN to Formula Mapping Not Found."
-            int_formula = str(None)
-
-        return int_formula
-    
 
 def calcIBB(SPN, formula):
     global ibb
     ibb = 0
-    for sublist in accts_bal:
+    global ibb_type
+    ibb_type = ' '
 
-        if sublist.spn == SPN:
-
+    for accts in accts_bal:
+        if accts.spn == SPN:
             if formula == "A":
                 print "Underlying Balance: OTE"
-                ibb = sublist.ote
+                ibb = accts.ote
+                ibb_type = "OTE"
             elif formula == "B":
                 print "Underlying Balance: TE"
-                ibb = sublist.te
+                ibb = accts.te
+                ibb_type = "TE"
             elif formula == "C":
                 print "Underlying Balance: Closing Balance"
-                ibb = sublist.closing_bal
+                ibb = accts.closing_bal
+                ibb_type = "Closing Bal"
             elif formula == 'D':
                 print "Underlying Balance: IM"
-                ibb = sublist.im
+                ibb = accts.im
+                ibb_type = "IM"
             elif formula == 'E':
                 print "Underlying Balance: Margin Exc/Def"    
-                ibb = sublist.med
+                ibb = accts.med
+                ibb_type = "MED"
             elif formula == 'F':
                 print "Underlying Balance: TE + Margin Exc/Def"
-                ibb = sublist.ote + sublist.med
+                ibb = accts.te + accts.med
+                ibb_type = "TE + MED"
             else:
                 print "Formula Calculation Method Not Found."
                 ibb = None
-           
+            
     return ibb
 
+def getFormula(SPN):
+    
+    print "CLIENT SPN:",SPN
+    
+    if SPN in SPNs.keys():
+        print "Formula:",SPNs[SPN][0]
+        int_formula = SPNs[SPN][0]
+    else:
+        print "SPN to Formula Mapping Not Found."
+        int_formula = str(None)
+    
+    return int_formula
 
 def getRate(SPN):
     
-    for sublist in SPNs:
-        if sublist.spn == SPN:
-            
-            print "Base Interest Rate:",int_base_rate
+    if SPN in SPNs.keys():
         
-            haircut = sublist.haircut
-            print "Haircut:",haircut
+        print "Base Interest Rate:",int_base_rate
         
-            final_rate = int_base_rate - haircut
-            print "DB/CR Interest Rate:",final_rate
-            return final_rate
+        haircut = SPNs[SPN][1]
+        print "Haircut:",haircut
         
-        else:
+        final_rate = int_base_rate - haircut
+        print "DB/CR Interest Rate:",final_rate
         
-            print "SPN to Rate Mapping Not Found."
+    else:
         
-            final_rate = str(None)
-            return final_rate
-    
+        print "SPN to Rate Mapping Not Found."
+        
+        final_rate = str(None)
+        
+    return final_rate
 
-def buildAccruedInterest(SPN, date, form, int_base_rate, rate, ibb, acc_int, versionID, call_type):
-
-    record = [str(SPN), str(date), form, int_base_rate, round(rate,2), round(ibb,2), round(acc_int,2), t1, str(call_type)]
+def buildAccruedInterest(SPN, date, form, int_base_rate, rate, ibb_type, ibb, acc_int, versionID, call_type):
+    hash = hashlib.sha1()
+    hash.update(str(versionID))
+    record = [str(SPN), str(date), form, int_base_rate, round(rate,2), ibb_type, round(ibb,2), round(acc_int,2), str(hash.hexdigest()), str(call_type)]
     table.append(record)
     
 def writeAccruedInterest(table):
@@ -190,7 +193,6 @@ def writeAccruedInterest(table):
         wr.writerow(row)
 
 def calcStdInterest(SPN):
-    global t1
     t1 = datetime.datetime.now()
     global call_type
     call_type = str(SPN)
@@ -204,14 +206,12 @@ def calcStdInterest(SPN):
     
     print date
     error = "Please check earlier messages. Missing Data. Interest Not Calculated."
-    version = 0
     
     if SPN.upper()=='*ALL':
-        for sublist in SPNs:
-            
-            form = getFormula(sublist.spn)
-            ibb = calcIBB(sublist.spn,form)
-            rate = getRate(sublist.spn)
+        for z in SPNs.keys():
+            form = getFormula(z)
+            ibb = calcIBB(z,form)
+            rate = getRate(z)
             if form == str(None) or ibb == str(None):
                 print error
                 pass
@@ -223,7 +223,7 @@ def calcStdInterest(SPN):
                     accrued_interest = 0
                     print "Negative Interest Value. Accrued Interest set to 0",'\n'  
             
-                buildAccruedInterest(sublist.spn, date, form, int_base_rate, rate, ibb, accrued_interest, version, call_type)
+                buildAccruedInterest(z, date, form, int_base_rate, rate, ibb_type, ibb, accrued_interest, t1, call_type)
             
     else:
             
@@ -241,7 +241,7 @@ def calcStdInterest(SPN):
                 accrued_interest = 0
                 print "Negative Interest Value. Accrued Interest set to 0",'\n'  
             
-            buildAccruedInterest(SPN, date, form, int_base_rate, rate, ibb, accrued_interest, version, call_type)
+            buildAccruedInterest(SPN, date, form, int_base_rate, rate, ibb_type, ibb, accrued_interest, t1, call_type)
             
     
     writeAccruedInterest(table)
@@ -250,13 +250,6 @@ def calcStdInterest(SPN):
 
 #tests for calcStdInterest
 
-# print calcStdInterest('12345A'),'\n'
-# print calcStdInterest('67890A'),'\n'
-# print calcStdInterest('89012A'),'\n'
-# print calcStdInterest('34567B'),'\n'
-# print calcStdInterest('45678F'),'\n'
-#print calcStdInterest('78901G'),'\n'
-#print calcStdInterest('777397X'),'\n'
-
 print calcStdInterest('*ALL'),'\n'
-#print calcStdInterest('30477AC')
+#print calcStdInterest('30067AC'),'\n'
+#print calcStdInterest('91696ACM')
