79d295638401300c846f4776d26725fe9bfb86ff
w0rkar0und <miten@mitenpatel.com>
2014-02-03 17:52:10 +0800

    Create Balances_Date_Currency.py

---
+++ Balances_Date_Currency.py
@@ -0,0 +1,25 @@
+'''
+Created on 07 Jan 14
+
+@author: R053016
+'''
+
+class Balances(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, data):
+        '''
+        Constructor
+        '''
+        self.spn = data[0].strip()
+        self.date = data[1]
+        self.currency = data[2]
+        self.ote = data[3]
+        self.closing_bal = data[4]
+        self.te = data[5]
+        self.im = data[6]
+        self.med = data[7]
+        
